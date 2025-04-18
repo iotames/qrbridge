8be4e4450aaf3e6bbdf1dfb1c2f330da3c8d7e15
Hankin <whq78164@gmail.com>
2025-04-18 11:57:28 +0800

    UPDATE: run as daemon

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/iotames/qrbridge/conf"
 	"github.com/iotames/qrbridge/db"
@@ -9,6 +11,35 @@ import (
 )
 
 func main() {
+	args := os.Args
+
+	for k, v := range args {
+		if v == "-d" || v == "--d" {
+			Daemon = true
+			args[k] = ""
+		}
+		if v == "stop" {
+			fmt.Println("删除功能不可用")
+			return
+		}
+
+	}
+	if Daemon {
+		fmt.Println("启动守护进程，后台运行程序")
+		var newArgs []string
+		if len(args) > 1 {
+			newArgs = args[1:]
+		}
+		cmd := exec.Command(args[0], newArgs...)
+		cmd.Env = os.Environ()
+		err := cmd.Start()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("守护进程启动成功，进程号：", cmd.Process.Pid)
+		return
+	}
+
 	defer db.DbClose()
 	if Dbinit {
 		dbInit()
--- main_args.go
+++ main_args.go
@@ -4,9 +4,10 @@ import (
 	"flag"
 )
 
-var Dbinit bool
+var Dbinit, Daemon bool
 
 func parseArgs() {
 	flag.BoolVar(&Dbinit, "init", false, "init db")
+	flag.BoolVar(&Daemon, "d", false, "run as daemon")
 	flag.Parse()
 }
